22bc69eb3d35aa037e5847871a05228effef0f50
Ali Soliman <alisolimanworks@gmail.com>
2023-08-14 10:53:27 +0300

    Updated Headers for Api Requests {from:website}

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Layout from "./pages/layout/Layout";
 import "./app.scss";
 import "./defaults.scss";
+import axios from "axios";
+axios.defaults.headers.common["from"] = "website";
 
 function App() {
   return (
