c578254a881fb139a5c9555107e1ae76e493408f
rustedmiind <alisolimanworks@gmail.com>
2024-02-15 18:30:01 +0200

    Fixed Designcard discount price not showing

--- src/pages/DesignService/components/design-card/DesignCard.tsx
+++ src/pages/DesignService/components/design-card/DesignCard.tsx
@@ -2,11 +2,20 @@ import ImageCard from "image-card/ImageCard";
 import { Design } from "types/Design/Design";
 
 function DesignCard({ design }: { design: Design }) {
+  let priceStartFrom: string | number | undefined;
+  if (design.price_before) {
+    if (design.price_after) {
+      priceStartFrom = design.price_after;
+    } else {
+      priceStartFrom = design.price_before;
+    }
+  }
+
   return (
     <ImageCard
       title={design.name || ""}
       description={design.desc}
-      price={`${design.price_before ? design.price_before + " ر.س." : ""}`}
+      price={`${priceStartFrom ? priceStartFrom + " ر.س." : ""}`}
       image={design.mainImage && design.mainImage[0]?.original_url}
       path={design.id?.toString()}
     />
