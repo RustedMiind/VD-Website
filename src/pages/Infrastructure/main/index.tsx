9f2531e857c964470897bbec474b64a83ca54c03
rustedmiind <alisolimanworks@gmail.com>
2024-03-06 01:44:50 +0200

    init infrastructure

---
+++ src/pages/Infrastructure/main/index.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "@mui/material";
+import PageBannerLayout from "pages/page-banner-layout/PageBannerLayout";
+
+function InfrastructureMainPage() {
+  return (
+    <Stack>
+      {/*
+      
+      -Custom Banner 
+        Navigation Filters
+
+      */}
+      {/* Search Component */}
+      {/* Cards Container */}
+    </Stack>
+  );
+}
+
+export default InfrastructureMainPage;
