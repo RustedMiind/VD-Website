46d5a0006ab05dd1a273b4af098f21e75bca5784
rustedmiind <alisolimanworks@gmail.com>
2024-03-06 15:31:49 +0200

    Infrastructure Project Details

--- src/components/GridImagesViewer/index.tsx
+++ src/components/GridImagesViewer/index.tsx
@@ -1,17 +1,37 @@
 import { Grid } from "@mui/material";
 import AspectRatioImageV2 from "components/AspectRatioImageV2";
+import { useState } from "react";
+import FsLightbox from "fslightbox-react";
 
 function GridImagesViewer({ images, limitRows = 2 }: PropsType) {
   const imagesToShow: GridImageItem[] = images.slice(0, limitRows * 2);
-
+  const [toggler, setToggler] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const openImage = (index: number) => {
+    setCurrentImageIndex(index);
+    setToggler(!toggler);
+  };
   return (
-    <Grid container spacing={1}>
-      {imagesToShow.map((img) => (
-        <Grid key={img.src} item xs={6}>
-          <AspectRatioImageV2 src={img.src} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={1}>
+        {imagesToShow.map((img, index) => (
+          <Grid key={img.src} item xs={6}>
+            <AspectRatioImageV2
+              src={img.src}
+              onClick={() => openImage(index)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
+      <FsLightbox
+        toggler={toggler}
+        sources={images.map((img) => (
+          <img alt="toggler" style={{ height: "90vh" }} src={img.src} />
+        ))}
+        sourceIndex={currentImageIndex}
+      />
+    </>
   );
 }
 
--- src/pages/Infrastructure/project/Media/ImagesGrid/index.tsx
+++ src/pages/Infrastructure/project/Media/ImagesGrid/index.tsx
@@ -8,22 +8,22 @@ function ImagesGrid() {
           src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
         },
         {
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          src: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
         },
         {
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          src: "https://img.freepik.com/premium-photo/glacier-mountain-landscape-with-flowers-lake-beautiful-sunset-with-full-moon_713888-1322.jpg",
         },
         {
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          src: "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
         },
         {
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          src: "https://deepai.org/static/images/cyberpunkdolphin.png",
         },
         {
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          src: "https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg",
         },
         {
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          src: "https://images.pexels.com/photos/220429/pexels-photo-220429.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         },
       ]}
     />
