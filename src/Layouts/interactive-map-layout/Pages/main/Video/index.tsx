d61da058ca05d89eb074b24e47c70dd4c623bc7a
rustedmiind <alisolimanworks@gmail.com>
2024-03-09 15:26:47 +0200

    Shadow for video

--- src/Layouts/interactive-map-layout/Pages/main/Video/index.tsx
+++ src/Layouts/interactive-map-layout/Pages/main/Video/index.tsx
@@ -9,26 +9,25 @@ function VideoComponent({ video }: PropsType) {
         width={{ xl: 850, lg: 700, md: 550, xs: 1 }}
         component={Paper}
         elevation={10}
+        sx={(theme) => ({
+          position: "relative",
+          ":before": {
+            content: '""',
+            zIndex: -10,
+            position: "absolute",
+            top: -20,
+            left: 20,
+            width: 1,
+            height: 1,
+            borderRadius: `${theme.shape.borderRadius}px`,
+            opacity: 0.7,
+            background:
+              "radial-gradient(76.87% 77.36% at 10.71% 10.1%, #78818D 0%, #422411 100%) ,linear-gradient(223.38deg, rgba(255, 255, 255, 0) 44.65%, rgba(255, 255, 255, 0.25) 48.69%, rgba(255, 255, 255, 0) 52.57%)",
+          },
+        })}
       >
         <AspectRatio ratio={16 / 9}>
-          <Box
-            sx={(theme) => ({
-              position: "relative",
-              // ":before": {
-              //   content: '""',
-              //   zIndex: -10,
-              //   position: "absolute",
-              //   top: -20,
-              //   left: 20,
-              //   width: 1,
-              //   height: 1,
-              //   borderRadius: `${theme.shape.borderRadius}px`,
-              //   opacity: 0.7,
-              //   background:
-              //     "radial-gradient(76.87% 77.36% at 10.71% 10.1%, #78818D 0%, #422411 100%) ,linear-gradient(223.38deg, rgba(255, 255, 255, 0) 44.65%, rgba(255, 255, 255, 0.25) 48.69%, rgba(255, 255, 255, 0) 52.57%)",
-              // },
-            })}
-          >
+          <Box>
             <Paper sx={{ overflow: "hidden", height: 1 }}>
               <ReactPlayer
                 key={video}
